2273fd22fc1f5c5fc99dd1c904dfff16479b51c2
André F <breakfastlp98@gmail.com>
2017-05-06 14:23:39 +0200

    First version.

---
+++ data.js
@@ -0,0 +1,17 @@
+angular.module('dataHandler', [])
+
+.controller('dataDisplay', function ($scope, $http) {
+  var URL = 'https://www.googleapis.com/youtube/v3/videos?part=statistics&id=4gSOMba1UdM&key=AIzaSyB-a6K5t_nTTWQTihPXYmCGW0pTKPdM4to';
+
+  $scope.data = {};
+
+  function getUserData() {
+    $http.get(URL).then(function (response) {
+      $scope.data = response.data.items[0].statistics;
+      document.getElementsByClassName('positiv')[0].style.width = (parseInt($scope.data.likeCount) / (parseInt($scope.data.likeCount) + parseInt($scope.data.dislikeCount))) * 100 + "%";
+      document.getElementsByClassName('negativ')[0].style.width = (parseInt($scope.data.dislikeCount) / (parseInt($scope.data.likeCount) + parseInt($scope.data.dislikeCount))) * 100 + "%";
+    });
+  }
+
+  setInterval(getUserData, 1000);
+});
